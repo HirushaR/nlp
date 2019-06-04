d13a69f41bdd0c823bbfa6a1b8499e07dd9bdd11
HirushR <hirusharandunu11@gmail.com>
2019-06-04 11:06:54 +0530

    add the regexp second part

---
+++ tokenize/regexp/R2.py
@@ -0,0 +1,24 @@
+#TODO:learn regexp
+import re;
+
+regex = re.compile(r'don\'t')
+fst  = "I don't go to school"
+sst =regex.sub("do not",fst)
+print(fst)
+print(sst)
+regex = re.compile(r'\'d')
+fst1  = "He'd go to school"
+sst1 =regex.sub("would",fst)
+print(fst1)
+print(sst1)
+
+line = " I won't go there, He's a mad man.He won't end that. He'd have to go now."
+givenpatterns = [(r'won\'t', 'will not'), (r'\'s', ' is'), (r'\'d', 'would')]
+
+def repalce(text, patterns):
+    for(raw, rep) in patterns:
+        regex1 = re.compile(raw)
+        text = regex1.sub(rep, text)
+    print(text)
+repalce(line, givenpatterns)
+
